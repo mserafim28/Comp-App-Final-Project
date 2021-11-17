2c8667b937c3202ebaafa7ebbed3d096591c047d
mariacgomes <gomesm@ufl.edu>
2021-11-17 16:58:47 -0500

    Create header.m

---
+++ header.m
@@ -0,0 +1,10 @@
+% BME 3053C Homework 2 MATLAB Header and Live Script
+%
+% Author: Maria C Gomes
+% Group Members: Maria Serafim, Gabriel Veliz
+% Course: BME 3053C Computer Applications for BME
+% Term: Fall 2021
+% J. Crayton Pruitt Family Department of Biomedical Engineering
+% University of Florida
+% Email: gomesm@ufl.edu
+% October 12, 2021
